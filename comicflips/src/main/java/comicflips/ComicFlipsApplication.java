1f5a9511b9715b797e74e2bfcbd7efa003d6d573
nihadhn2 <nihadhn2@gmail.com>
2019-03-20 15:06:06 -0400

    Initializing comic project

---
+++ comicflips/src/main/java/comicflips/ComicFlipsApplication.java
@@ -0,0 +1,13 @@
+package comicflips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicFlipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComicFlipsApplication.class, args);
+    }
+
+}
